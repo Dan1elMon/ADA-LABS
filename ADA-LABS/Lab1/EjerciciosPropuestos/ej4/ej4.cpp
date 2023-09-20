72cf4665572e07d6351381a76ffb59069128d74b
DanielMonroy <dchuramo@unsa.edu.pe>
2023-09-20 10:28:42 -0500

    avance

---
+++ ADA-LABS/Lab1/EjerciciosPropuestos/ej4/ej4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void myFunction();
+
+int main() {
+   int x;
+    int arr[5];
+    string list;
+    for (int i = 0; i < 5; i = i + 1) {
+        
+        cout << "Escriba un numero: ";
+        cin >> x;
+        arr[i]=x;   
+    } 
+
+    cout << "------------------";
+    myFunction(arr[5]);
+
+    return 0;
+}
+void myFunction(int arr) {
+    int min;
+  for(int i=0;i<5;i++){
+    if(arr[i]<arr[i]){
+        min=arr[i];
+    }
+  }
+  cout<<"El menor es";
+  cout<< min;
+}
