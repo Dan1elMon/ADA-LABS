706c8d72cd8d0e044cab4b23fa3c0867060c7c47
DanielMonroy <dchuramo@unsa.edu.pe>
2023-09-20 10:07:07 -0500

    ej1

---
+++ ADA-LABS/Lab1/EjerciciosPropuestos/ej1/ej1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y,z;
+  int prod;
+  cout << "Escriba un numero: ";
+  cin >> x;
+  cout << "Escriba un numero: ";
+  cin >> z;
+  cout << "Type another number: ";
+  cin >> y;
+  prod = x *y*z;
+  cout << "Elproducto es: " << prod;
+  return 0; 
+}
