69107399b40b898f725339b62c57ebabac9d920c
Daniel Chura <dchuramo@unsa.edu.pe>
2023-09-20 22:12:55 -0500

    ej2 termiado

--- ADA-LABS/Lab1/EjerciciosPropuestos/ej2/ej2.cpp
+++ ADA-LABS/Lab1/EjerciciosPropuestos/ej2/ej2.cpp
@@ -2,22 +2,26 @@
 using namespace std;
 
 int main() {
-    int x;
-    int arr[5];
-    string print;
-    for (int i = 0; i < 5; i = i + 1) {
-        
-        cout << "Escriba un numero: ";
-        cin >> x;
-        arr[i]=x;   
-    } 
+    const int MAX_ELEMENTOS = 100;
+    int numeros[MAX_ELEMENTOS];
+    int n;
 
-    cout << "------------------";
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> n;
 
-    for (int i = 0; i < 5; i = i + 1) {
-        
-        print= i+" -> "+arr[i];
-        cout << print;
-    } 
+    if (n <= 0 || n > MAX_ELEMENTOS) {
+        cout << "La cantidad de elementos debe ser mayor que 0 y menor o igual a " << MAX_ELEMENTOS << "." << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "Ingrese el elemento " << i << ": ";
+        cin >> numeros[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << i << " -> " << numeros[i] << endl;
+    }
+
     return 0;
 }
